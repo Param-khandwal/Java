2a4b53336c345939aae9cdce84b0509be14b353d
PARDEEP KUMAR <pramkhandwal1766@gmail.com>
2024-04-29 07:18:18 +0530

    updates

---
+++ Assignment 8/student1.java
@@ -0,0 +1,6 @@
+
+// 2. Modify program 1 to add a default constructor and a SetData() member 
+// function. 
+// -Create an array of 3 student using the default constructor and another 
+// array of 2 students using the parametrized constructor, and then display the 
+// data of all 5 course objects.	
---
+++ Assignment 9/person.java
@@ -0,0 +1,11 @@
+// 1. Define a base class person and a derived class employee with single inheritance. 
+// -Define SetData() member functions in each of the class with different signatures to set the data 
+// members and demonstrate overloading of member functions. 
+
+// -Define GetData() member functions in each of the class with same signatures to display data and 
+// demonstrate overriding of member functions. 
+
+
+
+
+
