9cedffa37a963a35b50c211e555f1849414cf0ff
PARDEEP KUMAR <pramkhandwal1766@gmail.com>
2024-05-02 07:27:32 +0530

    updates

---
+++ Assignment 10_(String Assignment)/Ques14.java
@@ -0,0 +1,6 @@
+14. Write a Java program to check whether the first two characters
+present at the end of a given string. 
+Sample Output:
+The given strings is: educated
+The first two characters appear in the last is: true
+
---
+++ Assignment 10_(String Assignment)/Ques15.java
@@ -0,0 +1,6 @@
+15.Write a Java program to check two given strings whether any oneof
+them appear at the end of the other string (ignore case sensitivity). Sample Output:
+The given strings are: xyz and pqrxyz
+Is one string appears at the end of other? True
+The given strings are: pqrxyz and xyz
+Is one string appears at the end of other? True
